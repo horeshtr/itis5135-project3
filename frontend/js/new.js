d4a8ca9dc2553e20afad79671b8db2ffe689df35
Travis Horesh <horeshtr@gmail.com>
2023-12-05 07:57:53 -0500

    Added form validity if statement to write to server or display error message.

--- frontend/js/new.js
+++ frontend/js/new.js
@@ -1,9 +1,10 @@
 "use strict";
 
-// get the post button and form elements from new.html
+// get the post button and form elements
 const postButton = document.querySelector("button[type=submit]");
 
 const form = document.querySelector("form");
+const requiredFields = form.querySelectorAll("[required]");
 const inputs = document.querySelectorAll("input");
 const textArea = document.querySelector("textarea");
 
@@ -20,11 +21,15 @@ const newPost = {id, title, author, date, profile, content};
 // set fetch url
 const url = `http://localhost:3000/blogs/?id=${id}`;
 
+// get the error notification elements
+const errorMessageContainer = document.querySelector(".notification-container");
+const errorMessageDiv = document.querySelector(".notification");
+
 // Add functionality to the post button
 form.addEventListener("submit", newPost);
 
 // check form validity, PUT new post to the server
-async function newPost(e) {
+async function createNewPost(e) {
   
   const isValid = form.reportValidity();
 
@@ -36,19 +41,37 @@ async function newPost(e) {
         const response = await fetch(url, {
             method: "PUT",
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(review)
+            body: JSON.stringify(newPost)
         });
 
         if (!response.ok) {
             throw Error(`Error ${response.url} ${response.statusText}`)
         }
         
-        window.location.href = "/";
+        //window.location.href = "/";
     } catch(error) {
         console.log(error.message)
     }
     
     // reset the form
     form.reset();
-  } 
+
+    // return to the landing page
+    window.location.href = "/index.html";
+
+  } else {
+    e.preventDefault();
+
+    errorMessageDiv.innerHTML = "";
+    errorMessageContainer.classList.remove("hidden");
+
+    requiredFields.forEach(field => {
+        if (field.value.trim() === "") {
+            const fieldName = field.getAttribute("name");
+            const errorMessage = document.createElement("p");
+            errorMessage.textContent = `${fieldName} is required, please correct the form.`;
+            errorMessageDiv.appendChild(errorMessage);
+        }
+    });
+  }
 }
